de2e32563131ad39dfd48a84bbe375aab4a123fa
achengli <tao3000ac@protonmail.com>
2025-04-12 03:38:46 +0200

    plug_install.sh fix

--- scripts/plug_install.sh
+++ scripts/plug_install.sh
@@ -3,19 +3,23 @@
 # Depending on git curl, bash, nvim and vim of course.
 
 function install-plug() {
-  if [ ! -f curl ]; 
+  if [ -z "$(whereis curl | cut -d: -f2)" ]; then
     echo 'The script deppends on curl. Install curl before.'
     return
   fi
 
-  if [ -f nvim ]; then
+  if [ -n "$(whereis nvim | cut -d: -f2)" ]; then
     curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim\
         --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  else
+    echo 'neovim installation not found'
   fi
 
-  if [ -f vim ]; then
+  if [ -n "$(whereis vim | cut -d: -f2)" ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  else
+    echo 'vim installation not found'
   fi
 }
 
